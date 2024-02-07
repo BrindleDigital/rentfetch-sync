579da304c2e9e38992a3bd3288d5da4e2209706b
Jon Schroeder <jon@elod.in>
2024-02-07 17:31:08 -0600

    Code standards

--- lib/initialization/check-missing-tables.php
+++ lib/initialization/check-missing-tables.php
@@ -2,7 +2,6 @@
 
 
 function rentfetch_check_actionscheduler_tables() {
-	
 	global $wpdb;
 
 	$table_list = array(
@@ -11,24 +10,29 @@ function rentfetch_check_actionscheduler_tables() {
 		'actionscheduler_groups',
 		'actionscheduler_claims',
 	);
-		
-	foreach( $table_list as $table ) {
-		
-		$found_tables = $wpdb->get_col( "SHOW TABLES LIKE '{$wpdb->prefix}{$table}'" );
-		
-		// if the tables are missing, output the notice and return
-		if ( !$found_tables ) {
-			
-			add_action( 'admin_notices', 'rentfetch_database_tables_missing_notice' );
-			
-			return;
+
+	$missing_tables = false; // Flag to track missing tables.
+
+	foreach ($table_list as $table) {
+		// Securely prefixing the table name.
+		$table_name = $wpdb->prefix . $table;
+		$query = $wpdb->prepare("SHOW TABLES LIKE %s", $table_name);
+		$found_tables = $wpdb->get_col($query);
+
+		if (empty($found_tables)) {
+			$missing_tables = true;
+			break; // Exit the loop if at least one table is missing.
 		}
+	}
+
+	if ($missing_tables) {
+		add_action('admin_notices', 'rentfetch_database_tables_missing_notice');
 	}
 }
 add_action( 'wp_loaded', 'rentfetch_check_actionscheduler_tables' );
 
 function rentfetch_database_tables_missing_notice() {
 	echo '<div class="notice notice-error is-dismissible">';
-	echo '<p>' . esc_html__( '<strong>Rent Fetch:</strong> The Action Scheduler tables appear to be missing. Please <a href="/wp-admin/tools.php?page=action-scheduler">vist the Action Scheduler admin page</a> to regenerate those.', 'rentfetch' ) . '</p>';
+	echo wp_kses_post( '<p>' . _x( '<strong>Rent Fetch:</strong> The Action Scheduler tables appear to be missing. Please <a href="/wp-admin/tools.php?page=action-scheduler">vist the Action Scheduler admin page</a> to regenerate those.', 'rentfetch' ) . '</p> ' );
 	echo '</div>';
 }
