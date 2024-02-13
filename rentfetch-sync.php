6c800ea725e095c7919667d6a3200c7416c11da4
Jon Schroeder <jon@elod.in>
2024-02-12 23:03:58 -0600

    Adding functionality to remove units that no longer appear in the API

--- rentfetch-sync.php
+++ rentfetch-sync.php
@@ -3,7 +3,7 @@
 	Plugin Name: Rent Fetch Sync
 	Plugin URI: https://github.com/jonschr/rentfetch-sync
 	Description: An addon for Rent Fetch that syncs properties, floorplans, and units 
-	Version: 0.3.5
+	Version: 0.3.6
 	Author: Brindle Digital
 	Author URI: https://www.brindledigital.com/
 
@@ -24,7 +24,7 @@ if ( !defined( 'ABSPATH' ) ) {
 }
 
 // Define the version of the plugin
-define ( 'RENTFETCHSYNC_VERSION', '0.3.5' );
+define ( 'RENTFETCHSYNC_VERSION', '0.3.6' );
 
 // Plugin directory
 define( 'RENTFETCHSYNC_DIR', plugin_dir_path( __FILE__ ) );
@@ -115,7 +115,7 @@ function rfs_start_sync_single_property() {
 	// RealPage doesn't have any property information or photos
 	
 	// define what to sync
-	rfs_sync_single_property( $property_id = 'p1671483', $integration = 'yardi' );
+	rfs_sync_single_property( $property_id = 'p1547922', $integration = 'yardi' );
 	
 }
 // add_action( 'wp_loaded', 'rfs_start_sync_single_property' );
