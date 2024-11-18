18389da21e831e50008b0163fa0852163229c11d
Jon Schroeder <jon@elod.in>
2024-11-18 12:16:33 -0600

    Set the transient better so that we're not constantly hitting the API if there's an error.

--- lib/api/fetch-rentfetch-api-info.php
+++ lib/api/fetch-rentfetch-api-info.php
@@ -20,13 +20,14 @@ function rfs_get_info_from_rentfetch_api() {
 	$transient = get_transient( 'rentfetch_api_info' );
 
 	if ( $transient ) {
+		// silence is golden.
 		return $transient;
 	}
 
 	$args = array(
 		'site_url'              => get_site_url(),
 		'site_name'             => get_bloginfo( 'name' ),
-		'current_date_time'     => current_time('mysql'),
+		'current_date_time'     => current_time( 'mysql' ),
 		'rentfetch_version'     => defined( 'RENTFETCH_VERSION' ) ? RENTFETCH_VERSION : 'unknown',
 		'rentfetchsync_version' => defined( 'RENTFETCHSYNC_VERSION' ) ? RENTFETCHSYNC_VERSION : 'unknown',
 		'wordpress_version'     => get_bloginfo( 'version' ),
@@ -57,10 +58,10 @@ function rfs_get_info_from_rentfetch_api() {
 
 	// turn the array into a json string.
 	$args = wp_json_encode( $args );
-	
+
 	// If RENTFETCH_API_URL is defined, constant('RENTFETCH_API_URL') retrieves its value and assigns it to $api_url.
 	// If RENTFETCH_API_URL is not defined, the default URL 'https://api.rentfetch.net/wp-json/rentfetchapi/v1/data' is assigned to $api_url.
-	$api_url = defined( 'RENTFETCH_API_URL' ) ? constant('RENTFETCH_API_URL') : 'https://api.rentfetch.net/wp-json/rentfetchapi/v1/data';
+	$api_url = defined( 'RENTFETCH_API_URL' ) ? constant( 'RENTFETCH_API_URL' ) : 'https://api.rentfetch.net/wp-json/rentfetchapi/v1/data';
 
 	$response = wp_remote_post(
 		$api_url,
@@ -74,13 +75,30 @@ function rfs_get_info_from_rentfetch_api() {
 	);
 
 	if ( is_wp_error( $response ) ) {
+		// When WordPress returns an error.
+
 		$error_message = $response->get_error_message();
+
+		set_transient( 'rentfetch_api_info', $error_message, 5 * MINUTE_IN_SECONDS );
+
 		return "Something went wrong: $error_message";
+	} elseif ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+		// When the response code is not 200.
+
+		$error_message = wp_remote_retrieve_response_message( $response );
+
+		set_transient( 'rentfetch_api_info', $error_message, 5 * MINUTE_IN_SECONDS );
+
+		return "Something went wrong: $error_message";
+
 	} else {
-		$response_php_array = json_decode( wp_remote_retrieve_body( $response ), true );
+		// When the response code is 200.
+
+		$body               = wp_remote_retrieve_body( $response ); // Retrieve response body.
+		$response_php_array = json_decode( $body, true );
 
 		// cache the response for 5 minutes.
-		set_transient( 'rentfetch_api_info', $response_php_array, 10 * MINUTE_IN_SECONDS );
+		set_transient( 'rentfetch_api_info', $response_php_array, 20 * MINUTE_IN_SECONDS );
 
 		return $response_php_array;
 	}
