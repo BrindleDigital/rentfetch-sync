3890267f1a98e79417ee079fe2db9867ee9b884c
Jon Schroeder <jon@elod.in>
2024-11-21 02:16:04 -0600

    Minor

--- rentfetch-sync.php
+++ rentfetch-sync.php
@@ -1,11 +1,11 @@
 <?php
 /*
-	Plugin Name: Rent Fetch Sync
-	Plugin URI: https://github.com/jonschr/rentfetch-sync
-	Description: An addon for Rent Fetch that syncs properties, floorplans, and units 
-	Version: 0.5.0
-	Author: Brindle Digital
-	Author URI: https://www.brindledigital.com/
+	Plugin Name:   Rent Fetch Sync
+	Plugin URI:    https://github.com/jonschr/rentfetch-sync
+	Description:   An addon for Rent Fetch that syncs properties, floorplans, and units
+	Version:       0.5.0
+	Author:        Brindle Digital
+	Author URI:    https://www.brindledigital.com/
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -19,20 +19,20 @@
 */
 
 /* Prevent direct access to the plugin */
-if ( !defined( 'ABSPATH' ) ) {
-	die( "Sorry, you are not allowed to access this page directly." );
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Sorry, you are not allowed to access this page directly.' );
 }
 
-// Define the version of the plugin
-define ( 'RENTFETCHSYNC_VERSION', '0.5.0' );
+// Define the version of the plugin.
+define( 'RENTFETCHSYNC_VERSION', '0.5.0' );
 
-// Plugin directory
+// Plugin directory.
 define( 'RENTFETCHSYNC_DIR', plugin_dir_path( __FILE__ ) );
 define( 'RENTFETCHSYNC_PATH', plugin_dir_url( __FILE__ ) );
 define( 'RENTFETCHSYNC_FILE', __FILE__ );
 define( 'RENTFETCHSYNC_SURECART_PUBLIC_TOKEN', 'pt_nw8Xnhfrs3tHBZZUFpdDRZ1q' );
 
-// Register deactivation hook
+// Register deactivation hook.
 register_deactivation_hook( __FILE__, 'rfs_deactivate_actions' );
 
 /**
@@ -45,17 +45,10 @@ function rfs_deactivate_actions() {
 	as_unschedule_all_actions( 'rfs_yardi_do_delete_orphans' );
 }
 
+// include action scheduler.
+require_once plugin_dir_path( __FILE__ ) . 'vendor/action-scheduler/action-scheduler.php';
 
-//////////////////////////////
-// INCLUDE ACTION SCHEDULER //
-//////////////////////////////
-
-require_once( plugin_dir_path( __FILE__ ) . 'vendor/action-scheduler/action-scheduler.php' );
-
-//////////////////////
-// INCLUDE SURECART //
-//////////////////////
-
+// include surecart licensing.
 if ( ! class_exists( 'SureCart\Licensing\Client' ) ) {
 	require_once RENTFETCHSYNC_DIR . '/vendor/surecart/src/Client.php';
 }
@@ -67,48 +60,45 @@ $client = new SureCart\Licensing\Client( 'Rent Fetch Sync', RENTFETCHSYNC_SURECA
 $client->set_textdomain( 'rentfetch-sync' );
 
 // add the pre-built license settings page.
-$client->settings()->add_page( 
-	[
-	'type'                 => 'submenu', // Can be: menu, options, submenu.
-	'parent_slug'          => 'rentfetch-options', // add your plugin menu slug.
-	'page_title'           => 'Manage Sync license',
-	'menu_title'           => 'Manage Sync license',
-	'capability'           => 'manage_options',
-	'menu_slug'            => $client->slug . '-manage-license',
-	'icon_url'             => '',
-	'position'             => null,
-	'activated_redirect'   => admin_url( 'admin.php?page=rentfetch-options' ), // should you want to redirect on activation of license.
+$client->settings()->add_page(
+	array(
+		'type'               => 'submenu', // Can be: menu, options, submenu.
+		'parent_slug'        => 'rentfetch-options', // add your plugin menu slug.
+		'page_title'         => 'Manage Sync license',
+		'menu_title'         => 'Manage Sync license',
+		'capability'         => 'manage_options',
+		'menu_slug'          => $client->slug . '-manage-license',
+		'icon_url'           => '',
+		'position'           => null,
+		'activated_redirect' => admin_url( 'admin.php?page=rentfetch-options' ), // should you want to redirect on activation of license.
 	// 'deactivated_redirect' => admin_url( 'admin.php?page=my-plugin-deactivation-page' ), // should you want to redirect on detactivation of license.
-	] 
+	)
 );
 
-// $activation = new SureCart\Licensing\Client( 'Rent Fetch Sync', 'pt_nw8Xnhfrs3tHBZZUFpdDRZ1q', __FILE__ );
-
-// print_r( $client );
-// echo $client->'public_token';
-// echo $client->'slug';
-
-///////////////////
-// FILE INCLUDES //
-///////////////////
-
-function rfs_require_files_recursive($directory) {
+/**
+ * Include all files in a directory and its subdirectories.
+ *
+ * @param   string $directory  The path to the directory to load.
+ *
+ * @return  void
+ */
+function rfs_require_files_recursive( $directory ) {
 	$iterator = new RecursiveIteratorIterator(
-		new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS),
+		new RecursiveDirectoryIterator( $directory, RecursiveDirectoryIterator::SKIP_DOTS ),
 		RecursiveIteratorIterator::LEAVES_ONLY
 	);
 
-	foreach ($iterator as $file) {
-		if ($file->isFile() && $file->getExtension() === 'php') {
+	foreach ( $iterator as $file ) {
+		if ( $file->isFile() && $file->getExtension() === 'php' ) {
 			require_once $file->getPathname();
 		}
 	}
 }
 
-// require_once all files in /lib and its subdirectories
-rfs_require_files_recursive(RENTFETCHSYNC_DIR . 'lib');
+// require_once all files in /lib and its subdirectories.
+rfs_require_files_recursive( RENTFETCHSYNC_DIR . 'lib' );
 
-// start the engine
+// start the engine.
 add_action( 'wp_loaded', 'rfs_perform_syncs' );
 
 // Load Plugin Update Checker.
