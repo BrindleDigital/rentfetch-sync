92b19cd1622b28ab25900f4013df8a5c9a66543a
elodin <jon@elod.in>
2025-06-11 15:37:46 -0500

    Adding the updateLeads API

--- lib/forms/shortcode-rentfetch_form.php
+++ lib/forms/shortcode-rentfetch_form.php
@@ -450,6 +450,57 @@ function rentfetch_send_lead_to_entrata( $form_data, $integration, $property_id 
 		$form_data['lead_source'] = '105949'; // Default lead source ID
 	}
 	
+	//! GETLEADS API to check if there's already a lead with this email address.
+	
+	// Set the URL for the API request.
+	$url = sprintf( 'https://apis.entrata.com/ext/orgs/%s/v1/leads', $subdomain );
+	
+	// Set the body for the request.
+	$getleads_body_array = array(
+		'auth'      => array(
+			'type' => 'apikey',
+		),
+		'requestId' => '15',
+		'method'    => array(
+			'name'   => 'getLeads',
+			'params' => array(
+				'propertyId' => $property_id,
+				'email' => $form_data['email'],
+				'excludeAmenities' => '1', // Exclude amenities from the response
+				'doNotSendConfirmationEmail' => '0',
+			),
+		),
+	);
+	
+	$getleads_body_json = wp_json_encode( $getleads_body_array );
+	
+	// Set the headers for the request.
+	$headers = array(
+		'X-Api-Key'    => $api_key,
+		'Content-Type' => 'application/json',
+	);
+
+	// Make the API request using wp_remote_post.
+	$getleads_response = wp_remote_post(
+		$url,
+		array(
+			'headers' => $headers,
+			'body'    => $getleads_body_json,
+			'timeout' => 10,
+		)
+	);
+	
+	// Retrieve and decode the response body.
+	$getleads_response_body = wp_remote_retrieve_body( $getleads_response );
+	
+	// let's decode the response body
+	$getleads_response_body = json_decode( $getleads_response_body, true );
+	
+	//! If there's an applicationId already, then we don't use the sendLeads API, but we'll instead use the updateLeads API.
+	$application_id = (int) $getleads_response_body['response']['result']['prospects'][0]['prospect'][0]['applicationId'] ?? null;
+	
+	//! SENDLEADS API if this is a brand new lead, or UPDATELEADS API if this is an existing lead with an applicationId.
+	
 	// Set the URL for the API request.
 	$url = sprintf( 'https://apis.entrata.com/ext/orgs/%s/v1/leads', $subdomain );
 	
@@ -460,9 +511,9 @@ function rentfetch_send_lead_to_entrata( $form_data, $integration, $property_id 
 		),
 		'requestId' => '15',
 		'method'    => array(
-			'name'   => 'sendLeads',
+			'name'   => $application_id ? 'updateLeads' : 'sendLeads',
 			'params' => array(
-				'propertyId' => $property_id,
+				'propertyId' => (int) $property_id,
 				'doNotSendConfirmationEmail' => '0',
 				'isWaitList' => '0',
 				'prospects' => array(
@@ -493,19 +544,33 @@ function rentfetch_send_lead_to_entrata( $form_data, $integration, $property_id 
 		),
 	);
 	
+	// If the applicationId is set, add it to the body
+	if ( ! empty( $application_id ) ) {
+		
+		// add the applicationId to the body.
+		$body_array['method']['params']['prospects']['prospect']['applicationId'] = $application_id;
+		
+		// add the version 'r2' to the method.
+		$body_array['method']['version'] = 'r2';
+		
+	}
+	
+	
 	// If  $form_data['schedule'] is set, add it to the body
 	if ( ! empty( $form_data['appointment_date'] ) && ! empty( $form_data['appointment_start_time'] ) && ! empty( $form_data['appointment_end_time'] ) ) {
 		$body_array['method']['params']['prospects']['prospect']['events']['event'] = array(
 			array(
 				'type'            => 'Appointment',
-				'eventTypeId'     => '17',
+				'eventTypeId'     => '17', // this is the nomenclature for sendLeads.
 				'subtypeId'       => '454',
 				'date'            => gmdate( 'm/d/Y\TH:i:s', strtotime( '-6 hours' ) ), // this API requires mountain time
 				'appointmentDate' => $form_data['appointment_date'],
 				'timeFrom'        => $form_data['appointment_start_time'],
 				'timeTo'          => $form_data['appointment_end_time'],
 				'eventReasons'    => 'Tour',
-				'comments'        => '',
+				'typeId'          => '17', // required for updateLeads.
+				'title'           => 'Appointment', // required for updateLeads.
+				'comments'        => 'n/a', // required to have a value for updateLeads. For sendLeads, a value is not required.
 			),
 		);
 	}
